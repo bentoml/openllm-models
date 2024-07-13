2007c0f22a41516797232fc6b1f8b31fd0b5e9f4
bojiang <bojiang_@outlook.com>
2024-07-13 10:03:33 +0800

    bump bentoml

--- bentoml/bentos/phi3/3.8b-instruct-ggml-q4-1078/src/bento_constants.py
+++ bentoml/bentos/phi3/3.8b-instruct-ggml-q4-1078/src/bento_constants.py
@@ -6,7 +6,7 @@ engine_config:
   model: microsoft/Phi-3-mini-4k-instruct-gguf
 extra_labels:
   model_name: microsoft/Phi-3-mini-4k-instruct-gguf
-  openllm_alias: 3.8b-q4,3.8b-mini-q4,3.8b-mini-instruct-4k-ggml-q4
+  openllm_alias: 3.8b-ggml-q4,3.8b-mini-instruct-4k-ggml-q4
 project: llamacpp-chat
 service_config:
   name: phi3
