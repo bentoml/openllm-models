a4d76280c19fc686554bea182c095ed69ca7addd
bojiang <bojiang_@outlook.com>
2024-09-25 20:10:13 +0800

    simplify

--- src/vllm-chat/service.py
+++ src/vllm-chat/service.py
@@ -1,10 +1,6 @@
-import functools
-from openai import AsyncOpenAI
-import PIL.Image
-import json
 import logging
 import os
-from typing import AsyncGenerator, Literal
+from typing import Literal
 
 import bentoml
 import fastapi
@@ -12,9 +8,7 @@ import fastapi.staticfiles
 import pydantic
 import vllm.entrypoints.openai.api_server as vllm_api_server
 import yaml
-from annotated_types import Ge, Le
 from fastapi.responses import FileResponse
-from typing_extensions import Annotated, Literal
 
 
 # Load the constants from the yaml file
@@ -24,7 +18,6 @@ with open(CONSTANT_YAML) as f:
 
 ENGINE_CONFIG = PARAMETERS.get("vllm", {}).get("engine_args", {})
 SERVICE_CONFIG = PARAMETERS.get("bentoml", {}).get("service_args", {})
-OVERRIDE_CHAT_TEMPLATE = PARAMETERS.get("chat_template")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -89,12 +82,6 @@ class VLLM:
         self.engine = AsyncLLMEngine.from_engine_args(ENGINE_ARGS)
         logger.info(f"VLLM service initialized with model: {ENGINE_CONFIG['model']}")
 
-        if OVERRIDE_CHAT_TEMPLATE:  # use community chat template
-            gen_config = _get_gen_config(OVERRIDE_CHAT_TEMPLATE)
-            chat_template = gen_config["template"]
-        else:
-            chat_template = None
-
         model_config = self.engine.engine.get_model_config()
 
         # inject the engine into the openai serving chat and completion
@@ -102,7 +89,7 @@ class VLLM:
             async_engine_client=self.engine,
             served_model_names=[ENGINE_CONFIG["model"]],
             response_role="assistant",
-            chat_template=chat_template,
+            chat_template=None,
             model_config=model_config,
             lora_modules=None,
             prompt_adapters=None,
@@ -116,61 +103,3 @@ class VLLM:
             prompt_adapters=None,
             request_logger=None,
         )
-
-    @bentoml.api(route="/api/chat")
-    async def chat(
-        self,
-        image: PIL.Image.Image,
-        prompt: str = "Describe the image",
-        max_tokens: Annotated[
-            int,
-            Ge(128),
-            Le(ENGINE_CONFIG["max_model_len"]),
-        ] = ENGINE_CONFIG["max_model_len"],
-    ) -> AsyncGenerator[str, None]:
-        """
-        light-weight chat API that takes in a list of messages and returns a response
-        """
-        import io
-        import base64
-    
-        img_byte_arr = io.BytesIO()
-        image.save(img_byte_arr, format='PNG')
-        img_byte_arr = img_byte_arr.getvalue()
-        img_b64_str = base64.b64encode(img_byte_arr).decode('utf-8')
-        img_type = 'image/png'
-    
-        client = AsyncOpenAI(base_url="http://localhost:3000/v1")
-        chat_completion = await client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "text", "text": prompt},
-                        {"type": "image_url", "image_url": {"url": f"data:{img_type};base64,{img_b64_str}"}},
-                    ]
-                },
-            ],
-            max_tokens=max_tokens,
-            model=ENGINE_CONFIG["model"],
-        )
-        async for message in chat_completion.messages:
-            yield message.content
-
-
-@functools.lru_cache(maxsize=1)
-def _get_gen_config(community_chat_template: str) -> dict:
-    logger.info(f"Load community_chat_template: {community_chat_template}")
-    chat_template_path = os.path.join(
-        os.path.dirname(__file__), "chat_templates", "chat_templates"
-    )
-    config_path = os.path.join(
-        os.path.dirname(__file__), "chat_templates", "generation_configs"
-    )
-    with open(os.path.join(config_path, f"{community_chat_template}.json")) as f:
-        gen_config = json.load(f)
-    chat_template_file = gen_config["chat_template"].split("/")[-1]
-    with open(os.path.join(chat_template_path, chat_template_file)) as f:
-        chat_template = f.read()
-    gen_config["template"] = chat_template.replace("    ", "").replace("\n", "")
-    return gen_config
