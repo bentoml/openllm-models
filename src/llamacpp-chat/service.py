2f27357e05cdbe5b96da5181a0a54018dbf0848b
xianml <xian@bentoml.com>
2024-11-15 16:27:57 +0800

    chore: add qwen2.5 coder

--- src/llamacpp-chat/service.py
+++ src/llamacpp-chat/service.py
@@ -77,7 +77,7 @@ if "prometheus_client" in sys.modules:
 @bentoml.mount_asgi_app(openai_api_app, path="/v1")
 @bentoml.service(**SERVICE_CONFIG)
 class LlamaCppChat:
-
+    model = bentoml.models.HuggingFaceModel(ENGINE_CONFIG["model"])
     def __init__(self) -> None:
         from llama_cpp import Llama
         self.llm = Llama.from_pretrained(
