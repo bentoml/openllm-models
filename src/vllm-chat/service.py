991bf8682c2d2b6f054e470a5cbb1e751a555c4c
Aaron Pham <contact@aarnphm.xyz>
2025-02-07 23:00:41 +0000

    fix: correct use server_config field
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- src/vllm-chat/service.py
+++ src/vllm-chat/service.py
@@ -94,7 +94,7 @@ class VLLM:
         args.enable_reasoning = False
         args.reasoning_parser = None
 
-        for key, value in SERVICE_CONFIG.items(): setattr(args, key, value)
+        for key, value in SERVER_CONFIG.items(): setattr(args, key, value)
 
         coro = vllm_api_server.init_app_state(self.engine, model_config, openai_api_app.state, args)
         asyncio.create_task(coro)
