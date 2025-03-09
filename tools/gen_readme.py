78b77fbf98de39b2f99683435f4170f0c0a2801b
Aaron Pham <contact@aarnphm.xyz>
2025-03-09 00:48:02 +0000

    chore: update generated readme scripts
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

---
+++ tools/gen_readme.py
@@ -0,0 +1,10 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "jinja2",
+#     "openllm",
+# ]
+# ///
+import subprocess, pathlib, sys, jinja2
+
+with ((GIT_ROOT:=pathlib.Path(__file__).parent.parent)/"README.md").open('w') as f: f.write(jinja2.Environment(loader=jinja2.FileSystemLoader((GIT_ROOT/"tools").__fspath__())).get_template('README.md.j2').render(model_list=subprocess.check_output([sys.executable, '-m', 'openllm', 'model', 'list'], env={'OPENLLM_TEST_REPO': '.'}).decode('utf-8')))
